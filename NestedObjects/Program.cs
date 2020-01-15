5bed9f6f5e1773a1521f430a172c6450781dbf71
roberthshields93 <roberthshields93@yahoo.com>
2020-01-15 14:57:38 -0800

    test code

--- NestedObjects/Program.cs
+++ NestedObjects/Program.cs
@@ -10,6 +10,26 @@ namespace NestedObjects
     {
         static void Main(string[] args)
         {
+            Student s = new Student();
+            
+            s.FullName = "bob";
+            s.FullName = s.FullName.ToUpper();
+
+            Course CPW212 = new Course()
+            {
+                CourseId = 1,
+                CourseTitle = "Advanced .NET",
+                Credits = 5,
+                Description = "C#"
+            };
+
+            s.Schedule.Add(CPW212);
+
+            List<Course> courses = s.Schedule;
+            courses.Add(CPW212);
+            courses.Add(CPW212);
+            Console.WriteLine();
+            Console.ReadKey();
         }
     }
 }
