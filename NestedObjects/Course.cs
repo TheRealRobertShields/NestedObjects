e81f4034d7f7ad78bdf5304a65a28b5c119ae4fe
roberthshields93 <roberthshields93@yahoo.com>
2020-01-15 14:17:13 -0800

    Added documentation for Course class properties.

--- NestedObjects/Course.cs
+++ NestedObjects/Course.cs
@@ -6,13 +6,26 @@ using System.Threading.Tasks;
 
 namespace NestedObjects
 {
+    /// <summary>
+    /// Represents a single college course.
+    /// </summary>
     public class Course
     {
-
+        /// <summary>
+        /// The name of the course.
+        /// </summary>
         public string CourseTitle { get; set; }
+        /// <summary>
+        /// The unique identifier for the course.
+        /// </summary>
         public int CourseId { get; set; }
+        /// <summary>
+        /// Brief summary of the course.
+        /// </summary>
         public string Description { get; set; }
+        /// <summary>
+        /// The number of college credits earned for a passing grade.
+        /// </summary>
         public byte Credits { get; set; }
-
     }
 }
